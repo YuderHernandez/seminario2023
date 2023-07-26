14d0be856c6fb08f4b3186d20f2745ea651f5ec0
Yuder <Yuderdavid@gmail.com>
2023-07-26 07:01:43 -0500

    se cambia URI por primary key1 en cosmos

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 var config = {}
 
-config.endpoint = 'https://dbseminario.documents.azure.com:443/'
+config.endpoint = 'https://dbseminario.documents.azure.com:443/;AccountKey=U8SJx4LmaLRS37ydsuyhUUCOYJYX2RK9XCU1ZqRO6gWBOvo80XvCSDKgkXcLDnC4HSwnRJu8kR0iACDbnKvzxw=='
 config.key = '3tDITNXb9ukoP3ATCSJ2ug12bbQ175NHuMKcEuhcCXPBN97CZtH4NYuGcjikmJ93R9hwk2EQ5iqpACDbNeKMRQ=='
 
 config.database = {
