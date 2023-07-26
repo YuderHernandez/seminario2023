fe29edf8041124abe5d3271a6ea5b9d5e8c6d2c6
Yuder <Yuderdavid@gmail.com>
2023-07-26 18:46:39 -0500

    prueba sin bd

--- config.js
+++ config.js
@@ -1,114 +1,114 @@
-require('dotenv').config()
-var config = {}
+// require('dotenv').config()
+// var config = {}
 
-config.endpoint = process.env.endpoint
-config.key = process.env.key
+// config.endpoint = process.env.endpoint
+// config.key = process.env.key
 
 
-config.database = {
-  id: 'ECCI_DATA'
-}
+// config.database = {
+//   id: 'ECCI_DATA'
+// }
 
-config.container = {
-  id: 'datos'
-}
-const fecha = Date.now()
-let serial = "s" + fecha
-let fechaReal = Date(fecha)
-config.items = { 
-  QC:{
-    id: serial,
-    partitionKey: "ladrillos",
-    Usuario: "Simon Bolivar",
-    Buenos: 980.1459,
-    Malos: 0.5,
-    Total: 10,
-    Sede: "Bogota",
-    Lote: fechaReal
+// config.container = {
+//   id: 'datos'
+// }
+// const fecha = Date.now()
+// let serial = "s" + fecha
+// let fechaReal = Date(fecha)
+// config.items = { 
+//   QC:{
+//     id: serial,
+//     partitionKey: "ladrillos",
+//     Usuario: "Simon Bolivar",
+//     Buenos: 980.1459,
+//     Malos: 0.5,
+//     Total: 10,
+//     Sede: "Bogota",
+//     Lote: fechaReal
     
-  },
-  SG_SST:{
-    Estado_Banda: "operando"
-
-  }
-}
-
-// config.items = {
-//   Andersen: {
-//     id: 'Anderson.4',
-//     Country: 'USA',
-//     partitionKey: 'meta',
-//     lastName: 'Andersen',
-//     parents: [
-//       {
-//         firstName: 'Thomas'
-//       },
-//       {
-//         firstName: 'Mary Kay'
-//       }
-//     ],
-//     children: [
-//       {
-//         firstName: 'Gabriel',
-//         gender: 'masculino',
-//         grade: 5,
-//         pets: [
-//           {
-//             givenName: 'Fluf',
-//             type: 'cat'
-//           }
-//         ]
-//       }
-//     ],
-//     address: {
-//       state: 'WA',
-//       county: 'King',
-//       city: 'Seattle'
-//     }
 //   },
-//   Wakefield: {
-//     id: 'Wakefield.7',
-//     partitionKey: 'Italy',
-//     Country: 'Italy',
-//     parents: [
-//       {
-//         familyName: 'Wakefield',
-//         firstName: 'Robin'
-//       },
-//       {
-//         familyName: 'Miller',
-//         firstName: 'Ben'
-//       }
-//     ],
-//     children: [
-//       {
-//         familyName: 'Merriam',
-//         firstName: 'Jesse',
-//         gender: 'female',
-//         grade: 8,
-//         pets: [
-//           {
-//             givenName: 'Goofy'
-//           },
-//           {
-//             givenName: 'Shadow'
-//           }
-//         ]
-//       },
-//       {
-//         familyName: 'Miller',
-//         firstName: 'Lisa',
-//         gender: 'female',
-//         grade: 1
-//       }
-//     ],
-//     address: {
-//       state: 'NY',
-//       county: 'Manhattan',
-//       city: 'NY'
-//     },
-//     isRegistered: false
+//   SG_SST:{
+//     Estado_Banda: "operando"
+
 //   }
 // }
 
-module.exports = config
+// // config.items = {
+// //   Andersen: {
+// //     id: 'Anderson.4',
+// //     Country: 'USA',
+// //     partitionKey: 'meta',
+// //     lastName: 'Andersen',
+// //     parents: [
+// //       {
+// //         firstName: 'Thomas'
+// //       },
+// //       {
+// //         firstName: 'Mary Kay'
+// //       }
+// //     ],
+// //     children: [
+// //       {
+// //         firstName: 'Gabriel',
+// //         gender: 'masculino',
+// //         grade: 5,
+// //         pets: [
+// //           {
+// //             givenName: 'Fluf',
+// //             type: 'cat'
+// //           }
+// //         ]
+// //       }
+// //     ],
+// //     address: {
+// //       state: 'WA',
+// //       county: 'King',
+// //       city: 'Seattle'
+// //     }
+// //   },
+// //   Wakefield: {
+// //     id: 'Wakefield.7',
+// //     partitionKey: 'Italy',
+// //     Country: 'Italy',
+// //     parents: [
+// //       {
+// //         familyName: 'Wakefield',
+// //         firstName: 'Robin'
+// //       },
+// //       {
+// //         familyName: 'Miller',
+// //         firstName: 'Ben'
+// //       }
+// //     ],
+// //     children: [
+// //       {
+// //         familyName: 'Merriam',
+// //         firstName: 'Jesse',
+// //         gender: 'female',
+// //         grade: 8,
+// //         pets: [
+// //           {
+// //             givenName: 'Goofy'
+// //           },
+// //           {
+// //             givenName: 'Shadow'
+// //           }
+// //         ]
+// //       },
+// //       {
+// //         familyName: 'Miller',
+// //         firstName: 'Lisa',
+// //         gender: 'female',
+// //         grade: 1
+// //       }
+// //     ],
+// //     address: {
+// //       state: 'NY',
+// //       county: 'Manhattan',
+// //       city: 'NY'
+// //     },
+// //     isRegistered: false
+// //   }
+// // }
+
+// module.exports = config
